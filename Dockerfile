7e9b734140d4795f5217d61940b661bfc4d738d6
asad <asad.lionpk@gmail.com>
2015-07-25 17:31:00 +0000

    use screen to share terminal

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM ubuntu:14.04.1
 
 RUN apt-get update -y
 RUN apt-get install -y python-setuptools python-dev build-essential libffi-dev libssl-dev
+RUN apt-get install screen nano
+run chmod 777 /var/run/screen/
 
 WORKDIR /opt
 ADD . /opt/app
@@ -13,6 +15,9 @@ RUN python setup.py install
 ADD docker/run.sh /opt/run.sh
 RUN chmod 777 /opt/run.sh
 
+WORKDIR /home
+
 EXPOSE 57575
 
 CMD ["/opt/run.sh"]
+#CMD ["screen","-d","-R","default"]
--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set password
-echo "root:${PASSWORD}" | chpasswd
+#echo "root:${PASSWORD}" | chpasswd
 
 if [ -z ${PORT} ]
 then
@@ -9,5 +9,7 @@ then
   /opt/app/butterfly.server.py --unsecure --host=0.0.0.0
 else
   echo "Starting on port: ${PORT}"
-  /opt/app/butterfly.server.py --unsecure --host=0.0.0.0 --port=${PORT}
+ # function exit() { clear;}
+  /opt/app/butterfly.server.py --unsecure --host=0.0.0.0 --port=${PORT} --login=false --motd="" --cmd="screen -x default" & cd /home & screen -d -R default -s /bin/bash
+  #screen -d -R default
 fi
